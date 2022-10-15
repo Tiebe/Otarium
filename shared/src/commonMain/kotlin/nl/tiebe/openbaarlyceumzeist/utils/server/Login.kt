66a138b169023ddfabf1caabcfa9ec7e73b97b56
Tiebe <tiebe.groosman@gmail.com>
2022-10-15 06:35:41 -0700

    move urls to one file because ios

--- shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Login.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Login.kt
@@ -1,15 +1,9 @@
 package nl.tiebe.openbaarlyceumzeist.utils.server
 
 import io.ktor.client.call.*
-import io.ktor.http.*
 import kotlinx.serialization.Serializable
 import nl.tiebe.magisterapi.api.requestPOST
 import nl.tiebe.magisterapi.response.TokenResponse
-/*
-
-val LOGIN_URL = URLBuilder(SERVER_URL).appendPathSegments("login").build()
-val EXCHANGE_URL = URLBuilder(LOGIN_URL).appendPathSegments("exchange").build()
-
 
 suspend fun exchangeUrl(loginRequest: LoginRequest): LoginResponse {
     val response = requestPOST(EXCHANGE_URL, loginRequest)
@@ -17,7 +11,6 @@ suspend fun exchangeUrl(loginRequest: LoginRequest): LoginResponse {
     return response.body()
 }
 
-*/
 
 
 
