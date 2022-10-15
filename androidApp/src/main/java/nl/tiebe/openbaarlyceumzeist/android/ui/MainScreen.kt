b7d77755c15dd335f4710b236030c3b2e69d5662
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-16 00:49:33 +0200

    Change to compose

---
+++ androidApp/src/main/java/nl/tiebe/openbaarlyceumzeist/android/ui/MainScreen.kt
@@ -0,0 +1,28 @@
+package nl.tiebe.openbaarlyceumzeist.android.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import nl.tiebe.openbaarlyceumzeist.android.R
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            stringResource(R.string.waiting_for_grades),
+            textAlign = TextAlign.Center
+        )
+    }
+}
---
+++ androidApp/src/main/java/nl/tiebe/openbaarlyceumzeist/android/ui/Navigation.kt
@@ -0,0 +1,95 @@
+package nl.tiebe.openbaarlyceumzeist.android.ui
+
+import android.annotation.SuppressLint
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import nl.tiebe.openbaarlyceumzeist.android.R
+
+
+@Composable
+fun NavHostController(navController: NavHostController, innerPadding: PaddingValues) {
+    NavHost(navController = navController, startDestination = "main", modifier = Modifier.padding(innerPadding)) {
+        composable("main") {
+            MainScreen()
+        }
+        composable("login") {
+            LoginScreen()
+        }
+    }
+}
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int) {
+    object Main : Screen("main", R.string.mainBarItem)
+    object Item2 : Screen("login", R.string.bar2)
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val items = listOf(
+        Screen.Main,
+        Screen.Item2,
+    )
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(stringResource(screen.resourceId)) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
+                                launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHostController(navController, innerPadding)
+    }
+}
+
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    BottomBar(navController)
+}
