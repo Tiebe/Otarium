55556a08d2b0f2a00f624c0fb6cab73042c26338
Tiebe <tiebe.groosman@gmail.com>
2022-10-16 14:01:54 +0200

    notifications and some ui shit

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,20 @@
 import SwiftUI
+import shared
+import UserNotifications
 
 @main
 struct iOSApp: App {
+    var main: Main = Main()
+    
+    init() {
+        main.setup()
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+            if let error = error {
+                print("Request Notifications failed: \(error), \(error.localizedDescription)")
+            }
+        }
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
