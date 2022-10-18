47c717ae1db9fba0c21fb7798b9f6a8a73ff06b3
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-18 21:23:26 +0200

    changes

--- shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Values.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Values.kt
@@ -2,7 +2,9 @@ package nl.tiebe.openbaarlyceumzeist.utils.server
 
 import io.ktor.http.*
 
-val SERVER_URL = Url("http://192.168.2.37:8080")
+val RELEASE_SERVER_URL = Url("https://otarium.groosman.nl")
+val DEBUG_SERVER_URL = Url("http://192.168.2.37:80")
+val SERVER_URL = RELEASE_SERVER_URL
 
 val DEVICE_URL = URLBuilder(SERVER_URL).appendPathSegments("device").build()
 val DEVICE_ADD_URL = URLBuilder(DEVICE_URL).appendPathSegments("add").build()
