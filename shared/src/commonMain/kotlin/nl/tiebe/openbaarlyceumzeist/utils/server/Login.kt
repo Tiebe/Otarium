484d74cc6786e3869ef5059594394e409f76c55a
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-18 15:22:13 +0200

    IT FINALLLYLYLYLYLY WORKS (FUCK YOU KOTLIN) (FUCK YOU KTOR) (FUCK YOU STANDARDIZED WEBSOCKET DESIGN SCHEME)

--- shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Login.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Login.kt
@@ -1,15 +1,44 @@
 package nl.tiebe.openbaarlyceumzeist.utils.server
 
-import io.ktor.client.call.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
 import nl.tiebe.magisterapi.api.account.LoginFlow
-import nl.tiebe.magisterapi.api.requestPOST
 import nl.tiebe.magisterapi.response.TokenResponse
 
 suspend fun exchangeUrl(loginRequest: LoginRequest): LoginResponse {
-    val response = requestPOST(EXCHANGE_URL, loginRequest)
+    var response: LoginResponse? = null
 
-    return response.body()
+    client.webSocket(host = EXCHANGE_URL.host, port = EXCHANGE_URL.port, path = EXCHANGE_URL.encodedPath) {
+        send(Json.encodeToString(loginRequest))
+
+        incoming.consumeEach { frame ->
+            println("Received frame: $frame")
+            if (frame is Frame.Text) {
+                val json = Json.parseToJsonElement(frame.readText()).jsonObject
+                if (json["type"].toString().toInt() == 1) {
+                    response = Json.decodeFromString<LoginResponse>(frame.readText())
+                }
+            } else if (frame is Frame.Close) {
+                if (frame.readReason()?.knownReason != CloseReason.Codes.NORMAL) {
+                    println("Error: ${frame.readReason()?.message}")
+                    throw Exception("Received: ${frame.readReason()?.message}")
+                }
+            }
+        }
+    }
+
+    while (response == null) {
+        // Wait for session to be initialized
+    }
+
+    return response!!
 }
 
 fun getUrl(): Pair<String, String> {
@@ -17,14 +46,10 @@ fun getUrl(): Pair<String, String> {
     return Pair(loginUrl.url, loginUrl.codeVerifier)
 }
 
-
-
-
-
 @Serializable
 data class LoginRequest(val code: String, val codeVerifier: String)
 @Serializable
-data class LoginResponse(val accessTokens: ServerTokens, val magisterTokens: TokenResponse, val tenantUrl: String)
+data class LoginResponse(val accessTokens: ServerTokens, val magisterTokens: TokenResponse, val tenantUrl: String, @Required val type: Int = 1)
 
 @Serializable
 data class RefreshRequest(val refreshToken: String)
