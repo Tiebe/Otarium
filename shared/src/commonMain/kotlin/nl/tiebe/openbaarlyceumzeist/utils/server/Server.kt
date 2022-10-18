fafd6606c7ac481890d0aab1ddcb444350743232
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-18 15:22:13 +0200

    IT FINALLLYLYLYLYLY WORKS (FUCK YOU KOTLIN) (FUCK YOU KTOR) (FUCK YOU STANDARDIZED WEBSOCKET DESIGN SCHEME)

--- shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Server.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/openbaarlyceumzeist/utils/server/Server.kt
@@ -1,7 +1,22 @@
 package nl.tiebe.openbaarlyceumzeist.utils.server
 
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import nl.tiebe.magisterapi.api.requestPOST
+
+val client = HttpClient {
+    install(WebSockets)
+    install(ContentNegotiation) {
+        json(json = Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        })
+    }
+}
 
 suspend fun sendFirebaseToken(accessToken: String, token: String) {
     requestPOST(DEVICE_ADD_URL, DeviceAddRequest(token), accessToken)
