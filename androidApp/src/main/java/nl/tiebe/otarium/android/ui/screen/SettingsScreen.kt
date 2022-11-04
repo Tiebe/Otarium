405ec52f9920050f08600e038954e4f61e35b864
Gulp1n <abeldenouden@hotmail.com>
2022-11-04 20:19:12 +0100

    commit

---
+++ androidApp/src/main/java/nl/tiebe/otarium/android/ui/screen/SettingsScreen.kt
@@ -0,0 +1,37 @@
+package nl.tiebe.otarium.android.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import nl.tiebe.otarium.android.R
+import nl.tiebe.otarium.android.ui.LabelledCheckBox
+import nl.tiebe.otarium.showAds
+
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = stringResource(id = R.string.settings_title),
+        textAlign = TextAlign.Center)
+        val checkedState = remember { mutableStateOf(showAds()) }
+        LabelledCheckBox(checked = checkedState.value, onCheckedChange = {
+            checkedState.value = it
+            showAds(checkedState.value)
+        }, label = stringResource(id = R.string.show_ads_checkbox))
+
+    }
+}
