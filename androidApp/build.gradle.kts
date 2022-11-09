e06f644fe2bdeab1985e818f712751b406c26df1
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-11-09 20:03:48 +0100

    commit

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -61,10 +61,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:21.3.0")
 
     implementation("androidx.compose.material3:material3:1.1.0-alpha01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0-alpha01")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha01")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.0-alpha01")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0-alpha01")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.compose.material:material:1.4.0-alpha01")
 
