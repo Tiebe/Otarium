257381b6fc38e15f11a3952736b83b63f1923665
Gulp1n <abeldenouden@hotmail.com>
2022-12-01 18:24:51 +0100

    files

---
+++ androidApp/src/main/java/nl/tiebe/otarium/android/ui/screen/grades/CalcScreen.kt
@@ -0,0 +1,15 @@
+package nl.tiebe.otarium.android.ui.screen.grades
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalcScreen() {
+    Column(modifier = Modifier
+        .fillMaxSize()) {
+        Text(text = "E")
+    }
+}
