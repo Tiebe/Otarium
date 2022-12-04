0cf84eab830e86c3e114857141ebd58a6fee3e6d
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-04 23:10:36 +0100

    i mean, somethings happening

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,17 +1,24 @@
-@file:Suppress("UNUSED_VARIABLE")
+@file:Suppress("UNUSED_VARIABLE", "OPT_IN_IS_NOT_ENABLED")
 
 import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.INT
 
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("org.jetbrains.compose") version "1.2.1"
     id("com.codingfeline.buildkonfig")
     kotlin("plugin.serialization") version "1.7.10"
+    id("dev.icerock.mobile.multiplatform-resources")
 }
 
 version = "1.0"
 val versionCode = 15
 
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.RequiresOptIn")
+}
+
+@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
 kotlin {
     android()
 
@@ -33,19 +40,35 @@ kotlin {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 implementation("com.russhwolf:multiplatform-settings-no-arg:0.9")
                 implementation("io.github.aakira:napier:2.6.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.runtime)
+                implementation(compose.animation)
+                implementation(compose.material3)
+
                 implementation("nl.tiebe:magisterapi:1.1.4")
+
+
+                implementation("com.github.ireward:compose-html:1.0.2")
+                implementation("ca.gosyer:accompanist-pager:0.25.2")
+                implementation("ca.gosyer:accompanist-pager-indicators:0.25.2")
+                implementation("ca.gosyer:accompanist-swiperefresh:0.25.2")
+
+                api("dev.icerock.moko:resources:0.20.1")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation("dev.icerock.moko:resources-test:0.20.1")
             }
         }
         val androidMain by getting {
@@ -54,6 +77,8 @@ kotlin {
                 implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
                 implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
                 implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+
+                api("dev.icerock.moko:resources-compose:0.20.1")
             }
         }
         val androidTest by getting
@@ -89,9 +114,12 @@ android {
         targetSdk = 33
     }
     namespace = "nl.tiebe.otarium"
+
+    sourceSets.getByName("main").res.srcDir(File(buildDir, "generated/moko/androidMain/res"))
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
 }
 
 buildkonfig {
@@ -100,4 +128,8 @@ buildkonfig {
     defaultConfigs {
         buildConfigField(INT, "versionCode", versionCode.toString())
     }
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = "nl.tiebe.otarium"
 }
---
+++ shared/src/iosMain/kotlin/nl/tiebe/otarium/ui/screen/grades/calculation/subject/BackHandler.kt
@@ -0,0 +1,6 @@
+package nl.tiebe.otarium.ui.screen.grades.calculation.subject
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun CustomBackHandler(onBack: () -> Unit) {}
