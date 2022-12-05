102b94bacf3d3f83cc27ef1ea4087f89e1b5aa80
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-05 12:06:49 +0100

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.12.0")
