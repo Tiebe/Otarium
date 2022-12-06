8de6e345cd88d6443fc8e9b8e4ae273c626aa5b0
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-06 16:39:47 +0100

    Revert "Revert "changes""
    This reverts commit d5d417eb6fd2f65c2e217f5f31ecf8dcb7ce2045.

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/Navigation.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/Navigation.kt
@@ -1,6 +1,8 @@
 package nl.tiebe.otarium.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -9,15 +11,18 @@ import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.StringResource
 import nl.tiebe.otarium.MR
 import nl.tiebe.otarium.showAds
+import nl.tiebe.otarium.ui.icons.CalendarTodayIcon
+import nl.tiebe.otarium.ui.icons.Looks10Icon
+import nl.tiebe.otarium.ui.icons.SettingsIcon
 import nl.tiebe.otarium.ui.screen.SettingsScreen
 import nl.tiebe.otarium.ui.screen.agenda.AgendaScreen
 import nl.tiebe.otarium.ui.screen.grades.GradeScreen
 import nl.tiebe.otarium.utils.getLocalizedString
 
 sealed class Screen(val resourceId: StringResource, val icon: @Composable () -> Unit) {
-    object Agenda : Screen(MR.strings.agendaItem, { /*Icon(painterResource(R.drawable.ic_baseline_calendar_today_24), "Timetable")*/ })
-    object Grades : Screen(MR.strings.gradesItem, { /*Icon(painterResource(R.drawable.ic_baseline_looks_10_24), "Grades")*/ })
-    object Settings : Screen(MR.strings.settings_title, { /*Icon(Icons.Filled.Settings, "Settings")*/ })
+    object Agenda : Screen(MR.strings.agendaItem, { Icon(CalendarTodayIcon, "Timetable") })
+    object Grades : Screen(MR.strings.gradesItem, { Icon(Looks10Icon, "Grades") })
+    object Settings : Screen(MR.strings.settings_title, { Icon(SettingsIcon, "Settings") })
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,6 +35,7 @@ fun BottomBar(screenState: MutableState<Screen>, modifier: Modifier) {
     )
     Scaffold(
         bottomBar = {
+            Icons.Default.CalendarToday
             NavigationBar(modifier = modifier, contentColor = MaterialTheme.colorScheme.onPrimary, containerColor = MaterialTheme.colorScheme.primary) {
                 items.forEach { screen ->
                     NavigationBarItem(
---
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/icons/CalendarTodayIcon.kt
@@ -0,0 +1,43 @@
+package nl.tiebe.otarium.ui.icons
+
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val CalendarTodayIcon: ImageVector
+    get() {
+        if (_calendarToday != null) {
+            return _calendarToday!!
+        }
+        _calendarToday = materialIcon(name = "Filled.CalendarToday") {
+            materialPath {
+                moveTo(20.0f, 3.0f)
+                horizontalLineToRelative(-1.0f)
+                lineTo(19.0f, 1.0f)
+                horizontalLineToRelative(-2.0f)
+                verticalLineToRelative(2.0f)
+                lineTo(7.0f, 3.0f)
+                lineTo(7.0f, 1.0f)
+                lineTo(5.0f, 1.0f)
+                verticalLineToRelative(2.0f)
+                lineTo(4.0f, 3.0f)
+                curveToRelative(-1.1f, 0.0f, -2.0f, 0.9f, -2.0f, 2.0f)
+                verticalLineToRelative(16.0f)
+                curveToRelative(0.0f, 1.1f, 0.9f, 2.0f, 2.0f, 2.0f)
+                horizontalLineToRelative(16.0f)
+                curveToRelative(1.1f, 0.0f, 2.0f, -0.9f, 2.0f, -2.0f)
+                lineTo(22.0f, 5.0f)
+                curveToRelative(0.0f, -1.1f, -0.9f, -2.0f, -2.0f, -2.0f)
+                close()
+                moveTo(20.0f, 21.0f)
+                lineTo(4.0f, 21.0f)
+                lineTo(4.0f, 8.0f)
+                horizontalLineToRelative(16.0f)
+                verticalLineToRelative(13.0f)
+                close()
+            }
+        }
+        return _calendarToday!!
+    }
+
+private var _calendarToday: ImageVector? = null
