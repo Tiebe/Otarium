2b2c682682c6362a012c338fd06a559b59777eac
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-06 16:39:47 +0100

    Revert "maybe"
    This reverts commit a948f2e2001b21bee3115782e40b959be5a2c765.

--- shared/src/iosMain/kotlin/nl/tiebe/otarium/utils/LocalizedStrings.kt
+++
@@ -1,9 +0,0 @@
-package nl.tiebe.otarium.utils
-
-import dev.icerock.moko.resources.StringResource
-import dev.icerock.moko.resources.desc.Resource
-import dev.icerock.moko.resources.desc.StringDesc
-
-actual fun getLocalizedString(string: StringResource): String {
-    return StringDesc.Resource(string).localized()
-}
