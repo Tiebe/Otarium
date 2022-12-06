d8893d4f1a46b2434bada529a34bd8fd8b65563f
Tiebe <tiebe.groosman@gmail.com>
2022-12-06 20:38:16 +0100

    Revert "Revert "i mean, somethings happening""
    This reverts commit 1b2eb36617e8df079b9c840073341edc94e5966b.

---
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/screen/agenda/DaySelector.kt
@@ -0,0 +1,94 @@
+package nl.tiebe.otarium.ui.screen.agenda
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.plus
+import nl.tiebe.otarium.ui.utils.pagerTabIndicatorOffset
+import kotlin.math.floor
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun DaySelector(
+    dayPagerState: PagerState,
+    days: List<String>,
+    selectedWeek: State<Int>,
+    firstDayOfWeek: LocalDate
+) {
+    val scope = rememberCoroutineScope()
+    val weekPagerState = rememberPagerState(100)
+
+    HorizontalPager(count = 200, state = weekPagerState) { week ->
+        TabRow(
+            selectedTabIndex = dayPagerState.currentPage - (dayPagerState.pageCount / 2),
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(
+                        week - 100,
+                        selectedWeek,
+                        dayPagerState,
+                        tabPositions
+                    )
+                )
+            }) {
+            days.forEachIndexed { index, title ->
+                Tab(
+                    selected = dayPagerState.currentPage == index && selectedWeek.value == week,
+                    onClick = {
+                        scope.launch {
+                            dayPagerState.animateScrollToPage((week-100)*days.size + index + (dayPagerState.pageCount / 2))
+                        }
+                    },
+                    text = {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(
+                                text = title,
+                                maxLines = 1,
+                                overflow = TextOverflow.Clip,
+                                textAlign = TextAlign.Center,
+                                fontSize = 13.sp
+                            )
+                            Text(
+                                text = firstDayOfWeek.plus(
+                                    (week - 100) * 7 + index,
+                                    DateTimeUnit.DAY
+                                ).toString()
+                                    .split("-").reversed().subList(0, 1).joinToString(),
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+
+                    }
+                )
+            }
+        }
+
+        var currentPage by remember { mutableStateOf(dayPagerState.currentPage) }
+
+        // update selected week when swiping days
+        LaunchedEffect(dayPagerState.currentPage) {
+                scope.launch {
+                    if (currentPage != dayPagerState.currentPage) {
+                        if (selectedWeek.value == floor(((dayPagerState.currentPage) - (dayPagerState.pageCount / 2)).toFloat() / days.size).toInt()) {
+                            weekPagerState.animateScrollToPage(selectedWeek.value + 100)
+                        }
+
+                        currentPage = dayPagerState.currentPage
+                    }
+                }
+        }
+    }
+}
