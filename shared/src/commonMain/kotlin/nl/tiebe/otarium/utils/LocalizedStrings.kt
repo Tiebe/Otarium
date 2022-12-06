3cd43e6ef49cc9179b1a4218fb6b790fd75fce29
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-06 16:39:52 +0100

    Revert "changes"
    This reverts commit 102b94bacf3d3f83cc27ef1ea4087f89e1b5aa80.

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/utils/LocalizedStrings.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/utils/LocalizedStrings.kt
@@ -1,13 +1,8 @@
 package nl.tiebe.otarium.utils
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import dev.icerock.moko.resources.AssetResource
 import dev.icerock.moko.resources.StringResource
 import nl.tiebe.magisterapi.utils.format
 
 expect fun getLocalizedString(string: StringResource): String
-@Composable
-expect fun getIcon(icon: AssetResource): Painter
 
 fun Float.format(digits: Int) = "%.${digits}f".format(this)
