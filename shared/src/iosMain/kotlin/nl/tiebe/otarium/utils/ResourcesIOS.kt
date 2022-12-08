e996b7890c4451812bb203cb24776db796f8b2ff
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-08 21:26:30 +0100

    fix shit

--- shared/src/iosMain/kotlin/nl/tiebe/otarium/utils/ResourcesIOS.kt
+++ shared/src/iosMain/kotlin/nl/tiebe/otarium/utils/ResourcesIOS.kt
@@ -1,10 +1,19 @@
 package nl.tiebe.otarium.utils
 
-
 import dev.icerock.moko.resources.StringResource
 import dev.icerock.moko.resources.desc.Resource
 import dev.icerock.moko.resources.desc.StringDesc
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
 
 actual fun getLocalizedString(string: StringResource): String {
     return StringDesc.Resource(string).localized()
+}
+
+actual fun Float.format(decimals: Int): String {
+    val formatter = NSNumberFormatter()
+    formatter.minimumFractionDigits = 0u
+    formatter.maximumFractionDigits = 2u
+    formatter.numberStyle = decimals //Decimal
+    return formatter.stringFromNumber(NSNumber(double))!!
 }
