31846ef0c41c57e36430f99aed0d8b0a7a5e327c
Tiebe <tiebe.groosman@gmail.com>
2022-12-09 20:16:33 +0100

    why workie no more

--- shared/src/iosMain/kotlin/nl/tiebe/otarium/ui/screen/LoginScreen.kt
+++ shared/src/iosMain/kotlin/nl/tiebe/otarium/ui/screen/LoginScreen.kt
@@ -2,9 +2,39 @@ package nl.tiebe.otarium.ui.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.cValue
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.ptr
+import nl.tiebe.otarium.ui.UIKitApplier
+import platform.CoreGraphics.CGRect
+import platform.CoreGraphics.CGRectMake
+import platform.CoreGraphics.CGRectZero
+import platform.Foundation.NSURL
+import platform.Foundation.NSURLRequest
+import platform.WebKit.WKWebView
+import platform.WebKit.WKWebViewConfiguration
 
 @Composable
 internal actual fun LoginScreen(onLogin: () -> Unit) {
-    //TODO: login screen ios
-    Text("Login screen")
+    ComposeNode<WKWebView, UIKitApplier>(
+        factory = {
+            val config = WKWebViewConfiguration()
+            val frame = cValue<CGRect>()
+            frame.place(CGRectZero.ptr)
+
+            object : WKWebView(frame = frame, configuration = config) {
+
+            }.apply {
+
+                val request = NSURLRequest(NSURL(string = "https://google.com"))
+
+                this.loadRequest(request)
+            }
+        },
+        update = {
+
+        }
+    )
 }
