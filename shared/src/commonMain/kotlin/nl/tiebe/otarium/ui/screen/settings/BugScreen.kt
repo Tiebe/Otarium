6f3f0f8d6a14a1d9b046f776e9231462ac720335
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-12 12:14:12 +0100

    explenation text

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/screen/settings/BugScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/screen/settings/BugScreen.kt
@@ -1,16 +1,15 @@
 package nl.tiebe.otarium.ui.screen.settings
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import moe.tlaster.precompose.navigation.BackHandler
+import nl.tiebe.otarium.MR
+import nl.tiebe.otarium.utils.getLocalizedString
 
 // TODO make it look better
 
@@ -24,6 +23,8 @@ internal fun BugScreen(onExit: () -> Unit) {
                 .fillMaxSize()
                 .padding(16.dp))
         {
+            Text(text = getLocalizedString(MR.strings.bug_text_1), Modifier.padding(bottom = 16.dp))
+            Divider()
             Row(modifier = Modifier
                 .fillMaxWidth(0.95f)
                 .height(70.dp),
@@ -34,6 +35,8 @@ internal fun BugScreen(onExit: () -> Unit) {
                     Text("Clear")
                 }
             }
+            Divider()
+            Text(text = getLocalizedString(MR.strings.bug_text_2), Modifier.padding(vertical = 16.dp))
         }
     }
 }
