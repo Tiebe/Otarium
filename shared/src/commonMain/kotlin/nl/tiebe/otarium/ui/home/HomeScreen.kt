38f8cf155bb0f065660bddae3cfed983a5dc42db
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-03-02 23:06:45 +0100

    work

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/HomeScreen.kt
@@ -1,8 +1,28 @@
 package nl.tiebe.otarium.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import nl.tiebe.otarium.oldui.navigation.Ads
+import nl.tiebe.otarium.oldui.navigation.adsShown
 
 @Composable
 fun HomeScreen(component: HomeComponent) {
+    BottomBar(
+        childStack,
+        componentContext,
+        navigation,
+        Modifier.padding(bottom = if (adsShown) 50.dp else 0.dp),
+        onNewUser
+    )
 
+    if (adsShown) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+            Ads()
+        }
+    }
 }
