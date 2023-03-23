0057d28debc38cd5a5e11ec41f6f6e3aba0a1727
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-03-23 23:25:18 +0100

    recent grades

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/recentgrades/RecentGradesChildComponent.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/recentgrades/RecentGradesChildComponent.kt
@@ -1,12 +1,46 @@
 package nl.tiebe.otarium.ui.home.grades.recentgrades
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
+import dev.tiebe.magisterapi.response.general.year.grades.RecentGrade
+import dev.tiebe.magisterapi.utils.MagisterException
+import kotlinx.coroutines.launch
+import nl.tiebe.otarium.Data
+import nl.tiebe.otarium.magister.getRecentGrades
 import nl.tiebe.otarium.ui.home.grades.GradesChildComponent
+import nl.tiebe.otarium.ui.root.componentCoroutineScope
 
 interface RecentGradesChildComponent : GradesChildComponent {
+    val refreshState: Value<Boolean>
+    val grades: Value<List<RecentGrade>>
+
+    fun refreshGrades()
+
 }
 
 class DefaultRecentGradesChildComponent(componentContext: ComponentContext) : RecentGradesChildComponent, ComponentContext by componentContext {
+    override val refreshState: MutableValue<Boolean> = MutableValue(false)
+    private val scope = componentCoroutineScope()
 
+    override val grades: MutableValue<List<RecentGrade>> = MutableValue(Data.selectedAccount.grades)
+
+    override fun refreshGrades() {
+        scope.launch {
+            refreshState.value = true
+            try {
+                grades.value = Data.selectedAccount.getRecentGrades()
+            } catch (e: MagisterException) {
+                e.printStackTrace()
+            } catch (_: Exception) {
+            }
+            refreshState.value = false
+        }
+    }
+
+
+    init {
+        refreshGrades()
+    }
 
 }
