39a5b1acdef6bbe5ddad15d2087618a6bbf2d6fb
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-03-27 20:55:17 +0200

    quick thing

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/timetable/item/TimetableItem.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/timetable/item/TimetableItem.kt
@@ -1,10 +1,14 @@
 package nl.tiebe.otarium.ui.home.timetable.item
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
