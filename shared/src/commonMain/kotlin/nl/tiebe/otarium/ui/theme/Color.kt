e51b005fa8751d095cc71041454ba7f04cf82f8a
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-03-27 20:55:17 +0200

    quick thing

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/theme/Color.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/theme/Color.kt
@@ -2,3 +2,11 @@ package nl.tiebe.otarium.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Blue80 = Color(0xFF0F86E4)
+val Blue40 = Color(0xFF6ACBF0)
+
+val Green80 = Color(0xFF1FC43B)
+val Green40 = Green80
+
+val Yellow80 = Color(0xFFC40808)
+val Yellow40 = Yellow80
