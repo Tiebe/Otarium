e76dae6f67996f7f734ba8d2b92ffdf6949fb278
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-10 19:52:48 +0200

    recent grade popout

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/recentgrades/RecentGradeItem.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/recentgrades/RecentGradeItem.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -15,13 +17,16 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.tiebe.magisterapi.response.general.year.grades.RecentGrade
+import kotlinx.datetime.toLocalDateTime
+import nl.tiebe.otarium.MR
 import nl.tiebe.otarium.oldui.utils.topBottomRectBorder
+import nl.tiebe.otarium.utils.ui.format
+import nl.tiebe.otarium.utils.ui.getLocalizedString
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun RecentGradeItem(component: RecentGradesChildComponent, grade: RecentGrade) {
     var showExtraInfo by remember { mutableStateOf(false) }
-
 
     ListItem(
         modifier = Modifier
@@ -58,6 +63,43 @@ internal fun RecentGradeItem(component: RecentGradesChildComponent, grade: Recen
     )
 
     AnimatedVisibility(visible = showExtraInfo, enter = expandVertically(), exit = shrinkVertically()) {
-        //todo: add extra info here
+        Column(modifier = Modifier.padding(start = 16.dp)) {
+            ListItem(
+                modifier = Modifier
+                    .topBottomRectBorder(brush = SolidColor(MaterialTheme.colorScheme.outline)),
+                headlineText = { Text(grade.grade) },
+                overlineText = {
+                    Text(getLocalizedString(MR.strings.grade))
+                }
+            )
+
+            ListItem(
+                modifier = Modifier
+                    .topBottomRectBorder(brush = SolidColor(MaterialTheme.colorScheme.outline)),
+                headlineText = { //parse date
+                    val date = grade.enteredOn.substring(0, 26).toLocalDateTime()
+
+                    Text("${date.dayOfMonth}-${date.monthNumber}-${date.year} ${date.hour}:${date.minute}:${date.second}")
+                },
+                overlineText = {
+                    Text(getLocalizedString(MR.strings.entered_on))
+                }
+            )
+
+            if (grade.weight != 0.0) {
+                val beforeAfterAverage = component.calculateAverageBeforeAfter(grade)
+
+                ListItem(
+                    modifier = Modifier
+                        .topBottomRectBorder(brush = SolidColor(MaterialTheme.colorScheme.outline)),
+                    headlineText = {
+                        Text("${beforeAfterAverage.first.format(2)} -> ${beforeAfterAverage.second.format(2)}")
+                    },
+                    overlineText = {
+                        Text(getLocalizedString(MR.strings.average))
+                    }
+                )
+            }
+        }
     }
 }
