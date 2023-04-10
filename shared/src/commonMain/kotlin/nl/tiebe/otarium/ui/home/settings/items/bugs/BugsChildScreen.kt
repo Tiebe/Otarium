2cb20c32cfff4708a1484bd1c9408b3e4b10041c
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-10 12:51:37 +0200

    fix some stuff

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/settings/items/bugs/BugsChildScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/settings/items/bugs/BugsChildScreen.kt
@@ -1,18 +1,15 @@
 package nl.tiebe.otarium.ui.home.settings.items.bugs
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import nl.tiebe.otarium.Data
 import nl.tiebe.otarium.MR
 import nl.tiebe.otarium.utils.ui.getLocalizedString
-import kotlin.random.Random
 
 @Composable
 internal fun BugsChildScreen(component: BugsChildComponent) {
@@ -25,29 +22,6 @@ internal fun BugsChildScreen(component: BugsChildComponent) {
             text = getLocalizedString(MR.strings.bug_text_1),
             modifier = Modifier.padding(bottom = 16.dp)
         )
-
-        Divider()
-
-        Row(modifier = Modifier
-            .fillMaxWidth(0.95f)
-            .height(70.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = "Remove random grade",
-                textAlign = TextAlign.Center)
-            Button(
-                onClick = {
-                    val account = Data.selectedAccount
-                    val savedGrades = account.fullGradeList.toMutableList()
-
-                    savedGrades.removeAt(Random.nextInt(savedGrades.size))
-                    account.fullGradeList = savedGrades
-
-                }) {
-                Text("remove")
-            }
-        }
 
         Divider()
 
