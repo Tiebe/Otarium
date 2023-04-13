fbe7ca387488fefcc770d1b0656addd72beeb6ec
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-13 10:25:21 +0200

    hopefully fix #38

--- androidApp/src/main/java/nl/tiebe/otarium/androidApp/MainActivity.kt
+++ androidApp/src/main/java/nl/tiebe/otarium/androidApp/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.arkivanov.decompose.defaultComponentContext
 import nl.tiebe.otarium.RootView
+import nl.tiebe.otarium.utils.refreshGradesBackground
+import nl.tiebe.otarium.utils.reloadTokensBackground
 import nl.tiebe.otarium.utils.ui.Android
 
 class MainActivity : AppCompatActivity() {
@@ -24,9 +26,9 @@ class MainActivity : AppCompatActivity() {
         // has to be set in code or in theme
         window.decorView.setBackgroundColor(Color.WHITE)
         window.statusBarColor = Color.parseColor("#0F86E4")
-/*
+
         reloadTokensBackground()
-        refreshGradesBackground()*/
+        refreshGradesBackground()
 
         val rootComponentContext = defaultComponentContext()
 
