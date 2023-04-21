33e28d269387be55ee7f8032297e5369bdd93f07
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-21 18:25:39 +0200

    background?

--- shared/src/iosMain/kotlin/nl/tiebe/otarium/ui/theme/setWindowTheme.kt
+++ shared/src/iosMain/kotlin/nl/tiebe/otarium/ui/theme/setWindowTheme.kt
@@ -1,10 +1,16 @@
 package nl.tiebe.otarium.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import nl.tiebe.otarium.darkModeState
 import nl.tiebe.otarium.utils.ui.IOS
+import platform.UIKit.UIApplication
 import platform.UIKit.backgroundColor
 import platform.UIKit.UIColor
+import platform.UIKit.UIStatusBarStyle
+import platform.UIKit.UIStatusBarStyleLightContent
+import platform.UIKit.UIStatusBarStyleDarkContent
+import platform.UIKit.setStatusBarStyle
 
 actual fun setWindowTheme(color: Color) {
-    IOS.window.backgroundColor = UIColor(red = color.red.toDouble(), green = color.green.toDouble(), blue = color.blue.toDouble(), alpha = color.alpha.toDouble())
+    UIApplication.sharedApplication.setStatusBarStyle(if(darkModeState.value) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent)
 }
