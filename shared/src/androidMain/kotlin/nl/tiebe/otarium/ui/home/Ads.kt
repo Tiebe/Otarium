82ab7420dd1b97e67f08e3d62507be938079cc14
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-25 19:15:52 +0200

    fix ads

--- shared/src/androidMain/kotlin/nl/tiebe/otarium/ui/home/Ads.kt
+++ shared/src/androidMain/kotlin/nl/tiebe/otarium/ui/home/Ads.kt
@@ -5,12 +5,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
 import nl.tiebe.otarium.BuildConfig
 import nl.tiebe.otarium.Data
+import java.util.*
 
 @SuppressLint("VisibleForTests")
 @Composable
@@ -22,7 +20,7 @@ internal actual fun Ads() {
 
             AdView(context).apply {
                 adUnitId =
-                    if (BuildConfig.DEBUG) "ca-app-pub-3940256099942544~3347511713" // test ads
+                    if (BuildConfig.DEBUG) "ca-app-pub-3940256099942544/6300978111" // test ads
                     else "ca-app-pub-1684141915170982/7736101438" // prod ads
                 setAdSize(AdSize.BANNER)
 
--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package nl.tiebe.otarium.ui.home
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -15,16 +14,14 @@ var adsShown = mutableStateOf(Data.showAds)
 
 @Composable
 internal fun HomeScreen(component: HomeComponent) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        BottomBar(
-            component,
-            Modifier.padding(bottom = if (adsShown.value) 50.dp else 0.dp)
-        )
+    BottomBar(
+        component,
+        Modifier.padding(bottom = if (adsShown.value) 50.dp else 0.dp)
+    )
 
-        if (adsShown.value) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
-                Ads()
-            }
+    if (adsShown.value) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+            Ads()
         }
     }
 }
