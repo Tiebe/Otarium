d1e0c3b89478468ff7b9f00bbb477b19e0ad118e
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-27 23:45:04 +0200

    remove accompanist dependency

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 
 object Version {
-    const val appVersion = "3.1.1"
-    const val appVersionCode = 27
+    const val appVersion = "3.1.2"
+    const val appVersionCode = 28
 
     const val magister = "1.1.7"
 
@@ -16,7 +16,6 @@ object Version {
     const val decompose = "2.0.0-compose-experimental-alpha-02"
     const val moko = "0.22.0"
     const val buildkonfig = "0.13.3"
-    const val accompanist = "0.25.2"
     const val gms = "4.3.15"
     const val firebase = "31.5.0"
     const val multiplatform_settings = "1.0.0"
