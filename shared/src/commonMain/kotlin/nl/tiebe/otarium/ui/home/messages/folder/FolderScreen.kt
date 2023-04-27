bd75f56782f202e9811b51c21b62c7a9171f1c5a
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-27 19:14:52 +0200

    quick update

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/folder/FolderScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/folder/FolderScreen.kt
@@ -2,7 +2,6 @@ package nl.tiebe.otarium.ui.home.messages.folder
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,7 +18,7 @@ internal fun FolderScreen(component: FolderComponent) {
         state = component.refreshState,
         onRefresh = component::refresh
     ) {
-        val scrollState = rememberScrollState()
+        val scrollState = component.scrollState
         val reachedEnd = derivedStateOf { scrollState.value == scrollState.maxValue }
 
         if (reachedEnd.value) {
--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/message/MessageScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/message/MessageScreen.kt
@@ -1,13 +1,22 @@
 package nl.tiebe.otarium.ui.home.messages.message
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import nl.tiebe.otarium.ui.utils.parseHtml
 
 @Composable
 internal fun MessageScreen(component: MessageComponent) {
-    val messageContent = component.message.subscribeAsState().value
+    val scrollState = rememberScrollState()
 
-    Text(messageContent.content)
+    Column(Modifier.verticalScroll(scrollState)) {
+        val messageContent = component.message.subscribeAsState().value
+
+        Text(messageContent.content.parseHtml())
+    }
 
 }
