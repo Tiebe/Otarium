99de3d64de157a43b03dec5d6afeea8153b44586
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-28 20:33:17 +0200

    progress

---
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/elo/children/studyguides/folder/StudyGuideFolderComponent.kt
@@ -0,0 +1,13 @@
+package nl.tiebe.otarium.ui.home.elo.children.studyguides.folder
+
+import com.arkivanov.decompose.ComponentContext
+
+interface StudyGuideFolderComponent {
+
+}
+
+class DefaultStudyGuideFolderComponent(componentContext: ComponentContext) : StudyGuideFolderComponent, ComponentContext by componentContext {
+
+
+
+}
---
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/elo/children/studyguides/folder/StudyGuideFolderScreen.kt
@@ -0,0 +1,8 @@
+package nl.tiebe.otarium.ui.home.elo.children.studyguides.folder
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun StudyGuideFolderScreen(component: StudyGuideFolderComponent) {
+
+}
