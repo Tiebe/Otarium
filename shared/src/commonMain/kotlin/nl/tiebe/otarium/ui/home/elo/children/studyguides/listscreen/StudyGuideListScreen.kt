1b1b475483f527c00904986ef1677ab4bdeb7b9b
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-04-29 14:21:31 +0200

    bugfixes

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/elo/children/studyguides/listscreen/StudyGuideListScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/elo/children/studyguides/listscreen/StudyGuideListScreen.kt
@@ -1,14 +1,17 @@
 package nl.tiebe.otarium.ui.home.elo.children.studyguides.listscreen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 
@@ -19,11 +22,19 @@ internal fun StudyGuideListScreen(component: StudyGuideListComponent) {
     val scrollState = rememberScrollState()
     val pullRefreshState = rememberPullRefreshState(component.isRefreshing.subscribeAsState().value, onRefresh = component::refreshStudyGuides)
 
-    Column(Modifier.fillMaxSize().verticalScroll(scrollState).pullRefresh(pullRefreshState)) {
-        studyGuideItems.forEach {
-            StudyGuideListItem(component, it)
+    Box(modifier = Modifier.fillMaxSize().pullRefresh(pullRefreshState)) {
+        Column(Modifier.fillMaxSize().verticalScroll(scrollState)) {
+            studyGuideItems.forEach {
+                StudyGuideListItem(component, it)
 
-            Divider()
+                Divider()
+            }
         }
+
+        PullRefreshIndicator(
+            refreshing = component.isRefreshing.subscribeAsState().value,
+            state = pullRefreshState,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
     }
 }
