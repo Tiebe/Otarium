6e42270286f9bd3f5f41341a5a58597d13553932
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-05-07 18:14:39 +0200

    release

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 
 object Version {
-    const val appVersion = "3.2.1"
-    const val appVersionCode = 29
+    const val appVersion = "3.2.2"
+    const val appVersionCode = 30
 
     const val magister = "1.1.8"
 
