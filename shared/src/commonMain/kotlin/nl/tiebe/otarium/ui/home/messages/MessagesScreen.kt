d7ec330872f8fa6b3b47a76943391536233b14ce
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-05-23 22:28:36 +0200

    commit

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/MessagesScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/messages/MessagesScreen.kt
@@ -1,19 +1,35 @@
 package nl.tiebe.otarium.ui.home.messages
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DismissDirection
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.*
+import androidx.compose.material.rememberDismissState
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.Child
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.pop
 import dev.icerock.moko.resources.compose.stringResource
 import nl.tiebe.otarium.MR
@@ -21,22 +37,52 @@ import nl.tiebe.otarium.ui.home.messages.folder.FolderScreen
 import nl.tiebe.otarium.ui.home.messages.message.MessageScreen
 import nl.tiebe.otarium.ui.home.messages.message.receiver.ReceiverInfoScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 internal fun MessagesScreen(component: MessagesComponent) {
-    Column {
-        val screen = component.childStack.subscribeAsState()
+    val screen = component.childStack.subscribeAsState()
 
-        val name = when (val child = screen.value.active.instance) {
-            is MessagesComponent.Child.FolderChild -> child.component.folder.name
-            is MessagesComponent.Child.MessageChild -> child.component.message.subscribeAsState().value.subject
-            else -> stringResource(MR.strings.messagesItem)
+    Box(modifier = Modifier.padding(start = 5.dp, end = 5.dp)) {
+        MessageScreenChild(component, screen, screen.value.items[0], false)
+
+        for (item in screen.value.items.subList(1, screen.value.items.size)) {
+            val state = rememberDismissState()
+
+            //pop on finish
+            if (state.isDismissed(DismissDirection.StartToEnd)) {
+                component.navigation.pop()
+            }
+
+            SwipeToDismiss(
+                state = state,
+                background = {
+                },
+                directions = setOf(DismissDirection.StartToEnd)
+            ) {
+                MessageScreenChild(component, screen, item)
+            }
         }
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MessageScreenChild(
+    component: MessagesComponent,
+    screen: State<ChildStack<MessagesComponent.Config, MessagesComponent.Child>>,
+    item: Child.Created<MessagesComponent.Config, MessagesComponent.Child>,
+    poppable: Boolean = screen.value.backStack.isNotEmpty()
+) {
+    val name = when (val child = item.instance) {
+        is MessagesComponent.Child.FolderChild -> child.component.folder.name
+        is MessagesComponent.Child.MessageChild -> child.component.message.subscribeAsState().value.subject
+        else -> stringResource(MR.strings.messagesItem)
+    }
+    Column {
         TopAppBar(
             title = { Text(name, overflow = TextOverflow.Ellipsis, maxLines = 1) },
             navigationIcon = {
-                if (screen.value.backStack.isNotEmpty()) {
+                if (poppable) {
                     IconButton(onClick = { component.navigation.pop() }) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                     }
@@ -45,8 +91,8 @@ internal fun MessagesScreen(component: MessagesComponent) {
             windowInsets = WindowInsets(0.dp)
         )
 
-        Box(modifier = Modifier.padding(start = 5.dp, end = 5.dp)) {
-            when (val child = screen.value.active.instance) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            when (val child = item.instance) {
                 is MessagesComponent.Child.FolderChild -> FolderScreen(child.component)
                 is MessagesComponent.Child.MainChild -> MessagesFolderSelectScreen(child.component)
                 is MessagesComponent.Child.MessageChild -> MessageScreen(child.component)
@@ -60,7 +106,10 @@ internal fun MessagesScreen(component: MessagesComponent) {
 @Composable
 internal fun MessagesFolderSelectScreen(component: MessagesComponent) {
     val foldersState = component.folders.subscribeAsState()
-    val refreshState = rememberPullRefreshState(refreshing = component.refreshState.subscribeAsState().value, onRefresh = component::getFolders)
+    val refreshState = rememberPullRefreshState(
+        refreshing = component.refreshState.subscribeAsState().value,
+        onRefresh = component::getFolders
+    )
 
     Box(modifier = Modifier.fillMaxSize().pullRefresh(refreshState)) {
         val folders = foldersState.value.filter { it.parentId == 0 }
@@ -72,6 +121,10 @@ internal fun MessagesFolderSelectScreen(component: MessagesComponent) {
             }
         }
 
-        PullRefreshIndicator(component.refreshState.subscribeAsState().value, refreshState, modifier = Modifier.align(Alignment.TopCenter))
+        PullRefreshIndicator(
+            component.refreshState.subscribeAsState().value,
+            refreshState,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
     }
 }
