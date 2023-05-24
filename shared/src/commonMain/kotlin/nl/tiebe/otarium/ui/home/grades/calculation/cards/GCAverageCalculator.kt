68e1f81493dbccfc3b2d5e609c5456236b3af691
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-05-24 19:03:18 +0200

    quick bugfix

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/calculation/cards/GCAverageCalculator.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/calculation/cards/GCAverageCalculator.kt
@@ -1,10 +1,27 @@
 package nl.tiebe.otarium.ui.home.grades.calculation.cards
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -102,7 +119,7 @@ internal fun GCAverageCalculator(grades: List<GradeWithGradeInfo>, manualGrades:
                 Text(
                     text = if (calculatedAverage != null) calculatedAverage?.format(Data.decimals) ?: "" else "",
                     style = MaterialTheme.typography.displayMedium,
-                    color = if ((calculatedAverage ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
+                    color = if (Data.markGrades && (calculatedAverage ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
                 )
 
                 Button(modifier = Modifier.padding(top = 5.dp), onClick = {
--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/calculation/subject/GCSubjectLIstItem.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/grades/calculation/subject/GCSubjectLIstItem.kt
@@ -1,9 +1,19 @@
 package nl.tiebe.otarium.ui.home.grades.calculation.subject
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,7 +50,7 @@ internal fun GradeListItem(grade: GradeWithGradeInfo) {
                     style = MaterialTheme.typography.displaySmall.copy(fontSize = 18.sp),
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    color = if ((grade.grade.grade?.replace(",", ".")?.toFloatOrNull() ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
+                    color = if (Data.markGrades && (grade.grade.grade?.replace(",", ".")?.toFloatOrNull() ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
                 )
 
                 Text(
@@ -77,7 +87,7 @@ internal fun ManualGradeListItem(grade: ManualGrade, component: GradeCalculation
                         style = MaterialTheme.typography.displaySmall.copy(fontSize = 18.sp),
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
-                        color = if ((grade.grade.replace(",", ".").toFloatOrNull() ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
+                        color = if (Data.markGrades && (grade.grade.replace(",", ".").toFloatOrNull() ?: 10.0f) < Data.passingGrade) MaterialTheme.colorScheme.error else Color.Unspecified,
                     )
 
                     Text(
