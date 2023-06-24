95e430c74e84008eb8f7e0e1e474e6520e03cc4b
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-06-24 02:45:55 +0200

    small changes

--- shared/src/commonMain/kotlin/dev/tiebe/otarium/ui/home/grades/GradeScreen.kt
+++ shared/src/commonMain/kotlin/dev/tiebe/otarium/ui/home/grades/GradeScreen.kt
@@ -4,14 +4,15 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -22,33 +23,20 @@ import dev.tiebe.otarium.MR
 import dev.tiebe.otarium.logic.root.home.children.grades.GradesComponent
 import dev.tiebe.otarium.ui.home.grades.calculation.screen.GradeCalculationChild
 import dev.tiebe.otarium.ui.home.grades.recentgrades.RecentGradesChild
-import dev.tiebe.otarium.ui.utils.tabIndicatorOffset
 import dev.tiebe.otarium.utils.ui.getLocalizedString
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun GradesScreen(component: GradesComponent) {
-    val pagerState = rememberPagerState()
+    var page by remember { mutableStateOf(0) }
     val coroutineScope = rememberCoroutineScope()
 
     Column(modifier = Modifier.fillMaxSize()) {
         TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.tabIndicatorOffset(
-                        pagerState,
-                        2,
-                        tabPositions,
-                        shouldShowIndicator = true,
-                        pagerState.currentPage
-                    )
-                )
-            }
+            selectedTabIndex = page
         ) {
             Tab(
-                selected = pagerState.currentPage == 0,
+                selected = page == 0,
                 onClick = { component.changeChild(GradesComponent.GradesChild.RecentGrades) },
                 modifier = Modifier.height(53.dp)
             ) {
@@ -64,7 +52,7 @@ internal fun GradesScreen(component: GradesComponent) {
             }
 
             Tab(
-                selected = pagerState.currentPage == 1,
+                selected = page == 1,
                 onClick = { component.changeChild(GradesComponent.GradesChild.Calculation) },
                 modifier = Modifier.height(53.dp)
             ) {
@@ -81,22 +69,13 @@ internal fun GradesScreen(component: GradesComponent) {
         }
 
 
-        HorizontalPager(
-            pageCount = 2,
-            state = pagerState,
-            beyondBoundsPageCount = 1,
-            modifier = Modifier.fillMaxSize(),
-        ) { page ->
-            when (page) {
-                0 -> RecentGradesChild(component.recentGradeComponent)
-                1 -> GradeCalculationChild(component.calculationChildComponent)
-            }
+        when (page) {
+            0 -> RecentGradesChild(component.recentGradeComponent)
+            1 -> GradeCalculationChild(component.calculationChildComponent)
         }
 
         component.currentPage.subscribe {
-            coroutineScope.launch {
-                pagerState.animateScrollToPage(it)
-            }
+            page = it
         }
     }
 }
