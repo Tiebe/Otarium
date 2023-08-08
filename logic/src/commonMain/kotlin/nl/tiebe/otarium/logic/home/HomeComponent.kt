5b1ba54b36bc858dd4da1dc5a00c9286417bb511
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-08-09 01:35:19 +0200

    more docs

--- logic/src/commonMain/kotlin/nl/tiebe/otarium/logic/home/HomeComponent.kt
+++ logic/src/commonMain/kotlin/nl/tiebe/otarium/logic/home/HomeComponent.kt
@@ -5,14 +5,24 @@ import com.arkivanov.decompose.router.slot.activate
 import nl.tiebe.otarium.logic.RootComponent
 
 interface HomeComponent {
+    /** The root component object. Should probably do this using DI one day. */
     val rootComponent: RootComponent
+
+    /** Slot navigation object. */
     val navigation: SlotNavigation<MenuItem>
 
+    /** A list of supported and visible menu items. */
     val menuItems: List<MenuItem>
 
+    /**
+     * Open the specified menu item.
+     *
+     * @param item The menu item.
+     */
     fun navigate(item: MenuItem) {
         navigation.activate(item)
     }
 
+    /** Menu items should extend this interface. */
     interface MenuItemComponent
 }
