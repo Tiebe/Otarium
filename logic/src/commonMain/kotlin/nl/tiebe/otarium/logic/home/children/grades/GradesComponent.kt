47556f695026321d8075bdfaa6f01b0cad3d5b6a
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-08-09 13:58:02 +0200

    grades docs

--- logic/src/commonMain/kotlin/nl/tiebe/otarium/logic/home/children/grades/GradesComponent.kt
+++ logic/src/commonMain/kotlin/nl/tiebe/otarium/logic/home/children/grades/GradesComponent.kt
@@ -1,28 +1,20 @@
 package nl.tiebe.otarium.logic.home.children.grades
 
-import com.arkivanov.decompose.value.Value
-import com.arkivanov.essenty.parcelable.Parcelable
-import com.arkivanov.essenty.parcelable.Parcelize
-import nl.tiebe.otarium.logic.root.home.HomeComponent
-import nl.tiebe.otarium.logic.root.home.children.grades.children.calculation.GradeCalculationChildComponent
-import nl.tiebe.otarium.logic.root.home.children.grades.children.recent.RecentGradesChildComponent
+import nl.tiebe.otarium.logic.home.HomeComponent
+import nl.tiebe.otarium.logic.home.children.grades.children.calculation.GradeCalculationChildComponent
+import nl.tiebe.otarium.logic.home.children.grades.children.recent.RecentGradesChildComponent
 
+
+/**
+ * Interface for the grades menu of the home screen.
+ */
 interface GradesComponent : HomeComponent.MenuItemComponent {
-    @Parcelize
-    sealed class GradesChild(val id: Int): Parcelable {
-        object RecentGrades : GradesChild(0)
-        object Calculation : GradesChild(1)
-
-        //todo: overzicht van alle cijfers
-
-    }
-
+    /** The recent grades menu. */
     val recentGradeComponent: RecentGradesChildComponent
+
+    /** The grade calculation menu. */
     val calculationChildComponent: GradeCalculationChildComponent
 
-    val currentPage: Value<Int>
-
-    fun changeChild(gradesChild: GradesChild)
-
+    /** The interface for submenus. */
     interface GradesChildComponent
 }
