10e48a3ffd9c69f494e0330abbfb26ed8b1e18ed
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-08-17 18:14:52 +0200

    commit, no clue what

---
+++ logic-magister/src/commonMain/kotlin/nl/tiebe/otarium/logic/magister/data/wrapper/elo/LearningResource.kt
@@ -0,0 +1,9 @@
+package nl.tiebe.otarium.logic.magister.data.wrapper.elo
+
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+
+interface LearningResource : Parcelable {
+    val id: Int
+
+}
---
+++ logic-magister/src/commonMain/kotlin/nl/tiebe/otarium/logic/magister/data/wrapper/typealiases.kt
@@ -0,0 +1,10 @@
+package nl.tiebe.otarium.logic.magister.data.wrapper
+
+typealias MagisterRecentGrade = dev.tiebe.magisterapi.response.general.year.grades.RecentGrade
+typealias RecentGradeWrapper = nl.tiebe.otarium.logic.data.wrapper.RecentGrade
+
+typealias MagisterSubject = dev.tiebe.magisterapi.response.general.year.grades.Subject
+typealias SubjectWrapper = nl.tiebe.otarium.logic.data.wrapper.Subject
+
+typealias MagisterTimetableItem = dev.tiebe.magisterapi.response.general.year.agenda.AgendaItem
+typealias TimetableItemWrapper = nl.tiebe.otarium.logic.data.wrapper.TimetableItem
