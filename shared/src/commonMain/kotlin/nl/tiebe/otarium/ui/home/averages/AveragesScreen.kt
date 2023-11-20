40110c02c6aa9c580f3e995581b20449f12999a1
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-11-21 00:59:24 +0100

    global averages

--- shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/averages/AveragesScreen.kt
+++ shared/src/commonMain/kotlin/nl/tiebe/otarium/ui/home/averages/AveragesScreen.kt
@@ -12,17 +12,21 @@ import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.*
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import dev.tiebe.magisterapi.response.general.year.grades.Subject
+import nl.tiebe.otarium.MR
 import nl.tiebe.otarium.logic.root.home.children.averages.AveragesComponent
 import nl.tiebe.otarium.logic.root.home.children.averages.children.AveragesListScreen
 import nl.tiebe.otarium.logic.root.home.children.averages.children.AveragesListScreenTopAppBar
 import nl.tiebe.otarium.ui.home.averages.subject.AverageSubjectPopup
 import nl.tiebe.otarium.ui.home.averages.subject.AverageSubjectPopupTopAppBar
+import nl.tiebe.otarium.utils.ui.getLocalizedString
 
 @OptIn(ExperimentalDecomposeApi::class)
 @Composable
 internal fun AveragesScreen(component: AveragesComponent) {
     Column(Modifier.fillMaxSize()) {
-        val subjects = component.gradesList.subscribeAsState().value.map { it.grade.subject }.distinct().sortedBy { it.description.lowercase() }
+        val subjects = listOf(Subject(-1, abbreviation = "global", description = getLocalizedString(MR.strings.global_averages), index = -1)) +
+                component.gradesList.subscribeAsState().value.map { it.grade.subject }.distinct().sortedBy { it.description.lowercase() }
 
         Scaffold(
             topBar = {
@@ -47,7 +51,7 @@ internal fun AveragesScreen(component: AveragesComponent) {
                         is AveragesComponent.Child.SubjectChild -> AverageSubjectPopup(
                             instance.component,
                             subjects.first { it.id == instance.subjectId },
-                            component.gradesList.subscribeAsState().value.filter { it.grade.subject.id == instance.subjectId}
+                            component.gradesList.subscribeAsState().value
                         )
                     }
                 }
