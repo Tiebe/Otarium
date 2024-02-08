84c29a1c3b57cb8dc2d4ad3fbeaab43eaee218ee
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-02-08 22:35:54 +0100

    start add wear os, do login

---
+++ androidApp/src/main/java/nl/tiebe/otarium/androidApp/WearListener.kt
@@ -0,0 +1,17 @@
+package nl.tiebe.otarium.androidApp
+
+import android.content.Intent
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.WearableListenerService
+
+class WearListener : WearableListenerService() {
+    override fun onMessageReceived(messageEvent: MessageEvent) {
+        val intent = Intent(applicationContext, WearLoginActivity::class.java)
+        intent.putExtra("uri", messageEvent.data)
+        intent.putExtra("node", messageEvent.sourceNodeId)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        startActivity(intent)
+    }
+
+}
---
+++ wearApp/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-parcelize")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "nl.tiebe.otarium.wear"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "nl.tiebe.otarium"
+        minSdk = 30
+        targetSdk = libs.versions.android.sdk.compile.get().toInt()
+        versionCode = libs.versions.app.version.code.get().toInt()
+        versionName = libs.versions.app.version.string.get()
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+
+    implementation(libs.play.services.wearable)
+    implementation(platform(libs.wear.compose.bom))
+    implementation(libs.compose.ui)
+//    implementation(libs.wear.tooling.preview)
+    implementation(libs.androidx.wear.compose.compose.material)
+    implementation(libs.androidx.wear.compose.compose.material3)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.activity)
+    implementation(libs.core.splashscreen)
+    implementation(libs.androidx.wear.phone.interactions)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.decompose.core)
+    implementation(libs.decompose.compose)
+
+    api(libs.moko.resources.core)
+    implementation(libs.magister.api)
+}
