637bb2d2217a5f2f51a2e103e3af2728cb72becc
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-02-10 13:41:36 +0100

    remove signing config

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -21,7 +21,6 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
